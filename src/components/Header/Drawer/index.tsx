687ba64ca859a12bdb7cfe372089a5a69a9bb27f
Artur Moreira <arturacm@gmail.com>
2023-06-01 14:34:56 -0300

    feat: add mobile menu overlay
    Now there is a page overlay when the menu is
    open. If the user clicks on this overlay, the
    menu will be closed. Also if the user press the
    escape key (for accessibility) the menu will close

--- src/components/Header/Drawer/index.tsx
+++ src/components/Header/Drawer/index.tsx
@@ -2,6 +2,7 @@ import style from './style.module.scss';
 import close from '/icons/cross.svg';
 import Button from '../../Button';
 import ButtonIcon from '../../ButtonIcon';
+import { useEffect, useRef } from 'react';
 
 const messages = {
   about: 'About',
@@ -17,8 +18,32 @@ interface DrawerProps {
 }
 
 const Drawer = ({ onClose, open }: DrawerProps) => {
-   return (
-    <div className={`${style.container} ${!open? style.vanishing: ''}` }>
+  const overlay = useRef<HTMLDivElement>(null)
+
+  useEffect(()=>{
+    const overlayElement = overlay.current
+
+    if(overlayElement){
+      overlayElement.addEventListener('click', onClose)
+    }
+
+    const handleEscape =  ({key} : KeyboardEvent) => {
+      if (key === "Escape") onClose()
+    }
+
+    window.addEventListener('keypress', handleEscape)
+
+    return () => {
+      if(overlayElement) overlayElement.removeEventListener('click', onClose)
+
+      window.removeEventListener('keydown', handleEscape)
+
+    }
+  },[onClose])
+
+  return (
+    <>
+      <div className={`${style.overlay} ${!open? style.vanishing: ''}`} ref={overlay}/>
       <div className={`${style.drawer} ${open? style.closing: ''}`}>
         <ButtonIcon onClick={onClose} src={close} width={40} height={40}/>
         <nav className={style.info}>
@@ -31,7 +56,7 @@ const Drawer = ({ onClose, open }: DrawerProps) => {
           <p className={style.signup}>{`${open}`}{messages.notMember} <a href='#'>{messages.signUp}</a></p>
         </nav>
       </div>
-    </div>
+    </>
   );
 };
 
