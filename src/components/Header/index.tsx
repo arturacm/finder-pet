df19f3e2622290354660b9ea7fac095772e4916d
Artur Moreira <arturacm@gmail.com>
2023-06-02 13:07:20 -0300

    feat: adds desktop and tablet layouts to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,18 @@ import Drawer from './Drawer';
 import FavoriteIndicator from './FavoriteIndicator';
 import style from './style.module.scss';
 import menu from '/icons/Menu.svg';
-import logo from '/logo-small.svg';
+import mobileLogo from '/logo-small.svg';
+import fullLogo from '/logo-full.svg';
 import ButtonIcon from '../ButtonIcon';
+
+const messages = {
+  intro: "let's explore your pet!",
+  about: 'About',
+  contact: 'Contact',
+  login: 'Login',
+  mobileLogo: 'Simplified logo',
+  fullLogo: 'Full logo',
+};
 
 const Header = (): React.ReactElement => {
   const [open, setOpen] = useState(false);
@@ -16,12 +26,29 @@ const Header = (): React.ReactElement => {
   return (
     <header className={style.container}>
       <nav className={style.logoContainer}>
-        <ButtonIcon onClick={handleOpen} src={menu} />
-        <img src={logo} />
-        <Drawer open={open} onClose={handleClose} />
+        <ButtonIcon onClick={handleOpen} src={menu} className={style.menu} />
+        <img
+          src={mobileLogo}
+          alt={messages.mobileLogo}
+          className={style.mobileLogo}
+        />
+        <img src={fullLogo} alt={messages.fullLogo} className={style.tablet} />
+        <p className={style.tablet}>{messages.intro}</p>
+        <Drawer open={open} onClose={handleClose} className={style.menu} />
       </nav>
       <nav className={style.links}>
+        <nav className={style.desktopLinks}>
+          <a href="#" className={style.desktopLinks}>
+            {messages.about}
+          </a>
+          <a href="#" className={style.desktopLinks}>
+            {messages.contact}
+          </a>
+        </nav>
         <FavoriteIndicator />
+        <a href="#" className={style.desktopLinks}>
+          {messages.login}
+        </a>
       </nav>
     </header>
   );
