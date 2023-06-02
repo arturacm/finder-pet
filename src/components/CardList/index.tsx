6e20e8c6dac62cfbcc461f5fb51f7a5281758900
Artur Moreira <arturacm@gmail.com>
2023-06-02 12:06:27 -0300

    fix: list without key
    now a new field for id is being used
    as a key

--- src/components/CardList/index.tsx
+++ src/components/CardList/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import style from './style.module.scss';
-import SaveToggle from './PetCard';
+import PetCard from './PetCard';
 
 const petsData = [
   {
+    id: '9dbdab4f-93ef-4af7-b496-01c19378b580',
+    name: 'Joshua',
+    image: '/mock/baxter.png',
+    breed: 'Golden Retriever',
+    age: 5,
+    weight: 12,
+    type: 'dog',
+  },
+  {
+    id: '5ec10d78-402c-4347-9210-bcf6e6049d11',
     name: 'Baxter',
     image: '/mock/baxter.png',
     breed: 'French bulldog',
@@ -12,16 +22,19 @@ const petsData = [
     type: 'dog',
   },
   {
-    name: 'Baxter',
-    image: '/mock/baxter.png',
-    breed: 'French bulldog',
+    id: '18c148f0-c097-4310-a4cd-a4dce09a7612',
+    name: 'Tartar Sauce',
+    image: 'https://itpetblog.com.br/wp-content/uploads/2019/07/grumpy-cat.jpg',
+    breed: 'Grumpy',
     age: 5,
     weight: 12,
-    type: 'dog',
+    type: 'cat',
   },
 ] as const;
 const CardList = (): React.ReactElement => {
-  const cards = petsData.map(props => <SaveToggle {...props} />);
+  const cards = petsData.map(({ id, ...props }) => (
+    <PetCard {...props} key={id} />
+  ));
 
   return <div className={style.cardList}>{cards}</div>;
 };
