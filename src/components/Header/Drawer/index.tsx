855ba9e9044385f258ad260cd95d8a160b1a8591
Artur Moreira <arturacm@gmail.com>
2023-06-02 13:05:48 -0300

    feat: add className prop to Drawer
    also adjusts its code styling using prettier

--- src/components/Header/Drawer/index.tsx
+++ src/components/Header/Drawer/index.tsx
@@ -9,54 +9,60 @@ const messages = {
   contact: 'Contact',
   signIn: 'Sign In',
   notMember: 'Not a member?',
-  signUp: 'Sign up now'
-}
+  signUp: 'Sign up now',
+};
 
 interface DrawerProps {
   open: boolean;
   onClose: () => void;
+  className?: string;
 }
 
-const Drawer = ({ onClose, open }: DrawerProps) => {
-  const overlay = useRef<HTMLDivElement>(null)
+const Drawer = ({ onClose, open, className }: DrawerProps) => {
+  const overlay = useRef<HTMLDivElement>(null);
 
-  useEffect(()=>{
-    const overlayElement = overlay.current
+  useEffect(() => {
+    const overlayElement = overlay.current;
 
-    if(overlayElement){
-      overlayElement.addEventListener('click', onClose)
+    if (overlayElement) {
+      overlayElement.addEventListener('click', onClose);
     }
 
-    const handleEscape =  ({key} : KeyboardEvent) => {
-      if (key === "Escape") onClose()
-    }
+    const handleEscape = ({ key }: KeyboardEvent) => {
+      if (key === 'Escape') onClose();
+    };
 
-    window.addEventListener('keypress', handleEscape)
+    window.addEventListener('keypress', handleEscape);
 
     return () => {
-      if(overlayElement) overlayElement.removeEventListener('click', onClose)
+      if (overlayElement) overlayElement.removeEventListener('click', onClose);
 
-      window.removeEventListener('keydown', handleEscape)
-
-    }
-  },[onClose])
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [onClose]);
 
   return (
-    <>
-      <div className={`${style.overlay} ${!open? style.vanishing: ''}`} ref={overlay}/>
-      <div className={`${style.drawer} ${open? style.closing: ''}`}>
-        <ButtonIcon onClick={onClose} src={close} width={40} height={40}/>
+    <div className={className}>
+      <div
+        className={`${style.overlay} ${!open ? style.vanishing : ''}`}
+        ref={overlay}
+      />
+      <div className={`${style.drawer} ${open ? style.closing : ''}`}>
+        <ButtonIcon onClick={onClose} src={close} width={40} height={40} />
         <nav className={style.info}>
-          <a href='#'>{messages.about}</a>
-          <a href='#'> {messages.contact}</a>
+          <a href="#">{messages.about}</a>
+          <a href="#"> {messages.contact}</a>
         </nav>
         <hr className={style.divider} />
         <nav className={style.signIn}>
           <Button>{messages.signIn}</Button>
-          <p className={style.signup}>{`${open}`}{messages.notMember} <a href='#'>{messages.signUp}</a></p>
+          <p className={style.signup}>
+            {`${open}`}
+            {messages.notMember} <a href="#">{messages.signUp}</a>
+          </p>
         </nav>
       </div>
-    </>
+    </div>
   );
 };
 
