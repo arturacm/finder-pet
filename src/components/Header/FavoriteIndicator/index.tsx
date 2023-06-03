da18832159b962fae575f5a4bb492604528a28b7
Artur Moreira <arturacm@gmail.com>
2023-06-03 13:51:02 -0300

    feat: add saved pets counter on header

--- src/components/Header/FavoriteIndicator/index.tsx
+++ src/components/Header/FavoriteIndicator/index.tsx
@@ -1,15 +1,33 @@
+import { useAppSelector } from '../../../redux/hooks';
+import { selectSavedPetsQuantity } from '../../../redux/savedPets/selectors';
 import style from './style.module.scss';
 import heartgray from '/icons/Heart-gray.svg';
+import heartred from '/icons/Heart-red.svg';
 
 const message = {
   save: 'save',
+  pets: 'pets',
+  pet: 'pet',
 };
 
 const FavoriteIndicator = () => {
+  const savedPetsQuantity = useAppSelector(selectSavedPetsQuantity);
+  const displayQuantity = savedPetsQuantity > 0;
+
   return (
-    <div className={style.container}>
-      <img src={heartgray} alt='heart' />
-      <span>{message.save}</span>
+    <div
+      className={`${style.favoriteIndicator} ${
+        displayQuantity ? style.displayQuantity : ''
+      }`}
+    >
+      <img src={displayQuantity ? heartred : heartgray} alt="heart" />
+      <span>
+        {displayQuantity
+          ? `${savedPetsQuantity} ${
+              savedPetsQuantity === 1 ? message.pet : message.pets
+            }`
+          : message.save}
+      </span>
     </div>
   );
 };
--- src/redux/savedPets/selectors.ts
+++ src/redux/savedPets/selectors.ts
@@ -1,4 +1,9 @@
 import { RootState } from '../store';
 
-export const selectSavedPet = (id: string) => (state: RootState) =>
-  state.savedPetsReducer.savedPets.includes(id);
+export const selectSavedPet =
+  (id: string) =>
+  (state: RootState): boolean =>
+    state.savedPetsReducer.savedPets.includes(id);
+
+export const selectSavedPetsQuantity = (state: RootState): number =>
+  state.savedPetsReducer.savedPets.length;
