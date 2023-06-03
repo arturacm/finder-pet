46e1e2ac63e005dd1de942c9aea254603964a18d
Artur Moreira <arturacm@gmail.com>
2023-06-03 14:00:00 -0300

    feat: persist savedPets data on browser
    now the localStorage is being used to persist
    the state data of savedPets

--- src/redux/savedPets/slice.ts
+++ src/redux/savedPets/slice.ts
@@ -1,12 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 
+const savedPetsStorageKey = 'finder-pet/savedPets';
+
+const persistData = (state: string[]) => {
+  localStorage.setItem(savedPetsStorageKey, JSON.stringify(state));
+};
+
 type SavedPetsState = {
   savedPets: Array<string>;
 };
 
 const initialState: SavedPetsState = {
-  savedPets: new Array<string>(),
+  savedPets: JSON.parse(localStorage.getItem(savedPetsStorageKey) ?? '[]'),
 };
 
 const savedPetsSlice = createSlice({
@@ -14,10 +20,14 @@ const savedPetsSlice = createSlice({
   initialState,
   reducers: {
     save: (state, { payload }: PayloadAction<string>) => {
-      if (!state.savedPets.includes(payload)) state.savedPets.push(payload);
+      if (!state.savedPets.includes(payload)) {
+        state.savedPets.push(payload);
+        persistData(state.savedPets);
+      }
     },
     remove: (state, { payload }: PayloadAction<string>) => {
       state.savedPets = state.savedPets.filter(id => id !== payload);
+      persistData(state.savedPets);
     },
   },
 });
