f93084d2286b6ab77c76d949f88f5b509ec123d7
Artur Moreira <arturacm@gmail.com>
2023-06-03 21:18:52 -0300

    feat: form validation for Sign Up
    now the fields from the Sign Up form are
    being validated using React-Hook-Form and
    zod. For now the result is being logged in
    the console

--- src/routes/SignUp/index.tsx
+++ src/routes/SignUp/index.tsx
@@ -7,6 +7,9 @@ import Button from '@/components/Button';
 import ButtonIcon from '@/components/ButtonIcon';
 import { useNavigate } from 'react-router-dom';
 import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const messages = {
   head: {
@@ -31,9 +34,35 @@ const messages = {
   },
 };
 
+const createUserFormSchema = z.object({
+  email: z
+    .string()
+    .nonempty('this field is required')
+    .email('Invalid email format'),
+  name: z.string().nonempty('this field is required'),
+  username: z.string().nonempty('this field is required'),
+  password: z
+    .string()
+    .nonempty('this field is required')
+    .min(6, 'Password should have minimum 6 characters'),
+});
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>;
+
 function SignUp() {
   const navigate = useNavigate();
   const handleBack = useCallback(() => navigate(-1), [navigate]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema),
+  });
+
+  const createUser = (data: CreateUserFormData) => console.log(data);
+
   return (
     <Main>
       <div className={style.container}>
@@ -57,16 +86,32 @@ function SignUp() {
             className={style.backButton}
             onClick={handleBack}
           />
-          <form className={style.form}>
+          <form className={style.form} onSubmit={handleSubmit(createUser)}>
             <h1>{messages.form.header}</h1>
-            <label htmlFor="email">{messages.form.email}</label>
-            <input id="email" />
-            <label htmlFor="name">{messages.form.name}</label>
-            <input id="name" />
-            <label htmlFor="username">{messages.form.username}</label>
-            <input id="username" />
-            <label htmlFor="password">{messages.form.password}</label>
-            <input id="password" />
+            <label htmlFor="email">
+              {messages.form.email}{' '}
+              {errors.email && <span>*{errors.email.message}</span>}
+            </label>
+            <input id="email" type="email" {...register('email')} />
+
+            <label htmlFor="name">
+              {messages.form.name}{' '}
+              {errors.name && <span>*{errors.name.message}</span>}
+            </label>
+            <input id="name" {...register('name')} />
+
+            <label htmlFor="username">
+              {messages.form.username}{' '}
+              {errors.username && <span>*{errors.username.message}</span>}
+            </label>
+            <input id="username" {...register('username')} />
+
+            <label htmlFor="password">
+              {messages.form.password}{' '}
+              {errors.password && <span>*{errors.password.message}</span>}
+            </label>
+            <input id="password" type="password" {...register('password')} />
+
             <p>{messages.form.termsOfService}</p>
             <Button>{messages.form.submit}</Button>
           </form>
