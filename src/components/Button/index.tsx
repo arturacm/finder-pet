af549ccc76ff916c1adf9d5be00f4fa7e9378521
Artur Moreira <arturacm@gmail.com>
2023-06-04 11:42:16 -0300

    refactor: custom Button Component props
    now the Button component has access to all
    button props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import style from './style.module.scss';
 
-const Button = ({ children }: React.PropsWithChildren): React.ReactElement => {
-  return <button className={style.button}>{children}</button>;
+const Button = ({
+  children,
+  className,
+  ...rest
+}: React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>): React.ReactElement => {
+  return (
+    <button className={`${style.button} ${className ?? ''}`} {...rest}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
