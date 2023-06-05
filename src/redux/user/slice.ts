2c77345270003e6f208cedd3926b1a53e137fd22
Artur Moreira <arturacm@gmail.com>
2023-06-05 10:06:41 -0300

    feat: email login
    initial implementation for email login
    using supabass auth and saving the credentials
    in the redux state

--- src/redux/user/slice.ts
+++ src/redux/user/slice.ts
@@ -1,25 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { RootState } from '../store'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import type { RootState } from '../store';
+import type { User, Session } from '@supabase/supabase-js';
 
 type UserState = {
-  user: boolean;
+  user: User | null;
+  session: Session | null;
 };
 
 const initialState: UserState = {
-  user: false,
+  user: null,
+  session: null,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    login: state => {
-      state.user = !state.user;
+    login: (state, { payload }: PayloadAction<UserState>) => {
+      state.user = payload.user;
+      state.session = payload.session;
     },
   },
 });
 
-export const selectUser = (state: RootState) => state.userReducer.user
+export const selectUser = (state: RootState) => state.userReducer.user;
 
 export const { login } = userSlice.actions;
 
--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -4,8 +4,13 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import LoginBanner from '@/components/LoginBanner';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import routes from '../routes';
+import { supabase } from '@/remote/superbase';
+import { useAppDispatch } from '@/redux/hooks';
+import { login } from '@/redux/user/slice';
+import { useState } from 'react';
+import type { AuthError } from '@supabase/supabase-js';
 
 const messages = {
   form: {
@@ -16,11 +21,14 @@ const messages = {
   },
   forgotPassword: 'Forgot your password?',
   notMember: 'Not a member? ',
-  signUp: 'Sign up now'
+  signUp: 'Sign up now',
 };
 
 const loginFormSchema = z.object({
-  username: z.string().nonempty('this field is required'),
+  email: z
+    .string()
+    .nonempty('this field is required')
+    .email('Invalid email format'),
   password: z
     .string()
     .nonempty('this field is required')
@@ -38,25 +46,38 @@ function Login() {
     resolver: zodResolver(loginFormSchema),
   });
 
-  const createUser = (data: LoginFormData) => console.log(data);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [error, setError] = useState<AuthError | null>(null);
+  const createUser = async (formData: LoginFormData) => {
+    const { data, error } = await supabase.auth.signInWithPassword(formData);
+
+    console.log('data', data, 'error', error);
+    if (!error) {
+      dispatch(login(data));
+      navigate(routes.home);
+    } else {
+      setError(error);
+    }
+  };
 
   return (
     <LoginBanner>
       <form className={style.form} onSubmit={handleSubmit(createUser)}>
         <h1>{messages.form.header}</h1>
 
-        <label htmlFor="username">
+        <label htmlFor="email">
           {messages.form.username}{' '}
-          {errors.username && <span>*{errors.username.message}</span>}
+          {errors.email && <span>*{errors.email.message}</span>}
         </label>
-        <input id="username" {...register('username')} />
+        <input id="email" {...register('email')} />
 
         <label htmlFor="password">
           {messages.form.password}{' '}
           {errors.password && <span>*{errors.password.message}</span>}
         </label>
         <input id="password" type="password" {...register('password')} />
-
+        {error && <span className={style.authError}>* {error.message}</span>}
         <Button type="submit">{messages.form.submit}</Button>
       </form>
       <a href="#" className={style.forgotPassword}>
